22781a5238adc4fd2a2722c4c11e5c7461daf48a
val-nvs <153785603+val-nvs@users.noreply.github.com>
2025-10-06 23:22:34 +0100

    edit

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyB-UPrXMblPcO1gcdD0GuEhBA8JGMpbGs4",
+    authDomain: "social-e02b4.firebaseapp.com",
+    databaseURL: "https://social-e02b4-default-rtdb.firebaseio.com",
+    projectId: "social-e02b4",
+    storageBucket: "social-e02b4.firebasestorage.app",
+    messagingSenderId: "64302705629",
+    appId: "1:64302705629:web:9090276dba0418cee6e434"
+};
